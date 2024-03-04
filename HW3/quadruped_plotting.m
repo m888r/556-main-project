ffad6d16f48b901a689024de9bfb06f7d7f39c74
mkuznets23 <77693398+mkuznets23@users.noreply.github.com>
2024-03-03 20:13:47 -0800

    updated plotting for angles and velocity

--- HW3/quadruped_plotting.m
+++ HW3/quadruped_plotting.m
@@ -21,9 +21,39 @@ for n = 1:4
     plot(out.u.Time, us(3*n, :));
     xlabel('t [s]'); ylabel('\tau [Nm]'); legend('Hip', 'Thigh', 'Calf');
     title(leg(n));
+    ylim([-40 68])
 end
 
 % Joint Angle Plots
-
+figure("Name", "Leg Joint Angle Plots");
+qs = transpose(squeeze(out.q.Data));
+for n = 1:4
+    subplot(2, 2, n);
+    % Hip
+    plot(out.q.Time, qs(3*n - 2, :));
+    hold on;
+    % Thigh
+    plot(out.q.Time, qs(3*n - 1, :));
+    % Calf
+    plot(out.q.Time, qs(3*n, :));
+    xlabel('t [s]'); ylabel('\theta [rad]'); legend('Hip', 'Thigh', 'Calf');
+    title(leg(n))
+    ylim([-3 2])
+end
 
 % Joint Velocity Plots
+figure("Name", "Leg Joint Velocity Plots");
+dqs = transpose(squeeze(out.dq.Data));
+for n = 1:4
+    subplot(2, 2, n);
+    % Hip
+    plot(out.dq.Time, dqs(3*n - 2, :));
+    hold on;
+    % Thigh
+    plot(out.dq.Time, dqs(3*n - 1, :));
+    % Calf
+    plot(out.dq.Time, dqs(3*n, :));
+    xlabel('t [s]'); ylabel('\omega [rad/s]'); legend('Hip', 'Thigh', 'Calf');
+    title(leg(n));
+    ylim([-2.8 6])
+end
