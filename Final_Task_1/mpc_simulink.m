63b074fc4e1ee5656a37c531112c8c1a3b0dd870
liyvonne <79174603+liyvonne@users.noreply.github.com>
2024-04-18 15:55:10 -0700

    mpc troubleshooting files

---
+++ Final_Task_1/mpc_simulink.m
@@ -0,0 +1,130 @@
+function grf_forces = mpc_simulink(X, pf) 
+    Xd = [0; 0; 0.3; zeros(3,1); zeros(3,1); zeros(3,1)];
+    P = X(1:3);
+    ypr = X(4:6);
+    X_bard = [Xd; 9.81];
+    X_bar = [X; 9.81];
+    forces = 4;
+    bar_states = 13;
+    N = 10;
+    dt = 0.03;
+    Xmpc_elem = bar_states*N + forces*3*N;
+
+
+    % Q, R values from HW4 #2(b)
+    Q = diag([40, 50, 60, 10, 10, 10, 4, 4, 4, 1, 1, 1, 0]);
+    R = 0.00001*eye(forces*3);
+    m = 12;
+    F_max = 500;
+    F_min = 10;
+    mu = 0.5;
+    I_b = diag([0.0168, 0.0565, 0.064]);
+        
+    % Foot Contact
+    % Would need to update for swing trajectory
+%{
+ftcontact_prev = params.ftcontact_prev;
+    ftcontact_next = gait(1:forces);
+    hip_vec = [params.vk, params.vg, params.vi, params.ve];
+    for ind = 1:forces
+        if ftcontact_prev(ind) == 0 && ftcontact_next(ind) == 1
+            r_temp = R*hip_vec(:, ind) + P;
+            switch ind
+                case 1
+                    params.p_f1 = [r_temp(1:2); 0];
+                case 2
+                    params.p_f2 = [r_temp(1:2); 0];
+                case 3
+                    params.p_f3 = [r_temp(1:2); 0];
+                case 4
+                    params.p_f4 = [r_temp(1:2); 0];
+            end
+        end
+    end
+%}
+
+
+    % Cost function parameters
+    temp_fQs = [];
+    temp_Qs = [];
+    temp_Rs = [];
+    for ind = 1:N
+        temp_Qs = [temp_Qs; diag(Q)];
+        temp_Rs = [temp_Rs; diag(R)];
+        temp_fQs = [temp_fQs; -Q'*X_bard];
+    end
+    H = diag([temp_Qs; temp_Rs]);
+    f = [temp_fQs; zeros(3*forces*N, 1)];
+
+    % Inequality constraints A_iq
+    Fiq_mat = [-1, 0, -mu; 1, 0, -mu; 0, -1, -mu; 0, 1, -mu; 0, 0, 1; 0, 0, -1];
+    A_iq = [zeros(6*forces*N, bar_states*N), kron(eye(forces*N),Fiq_mat)];
+
+
+    % Inequality constraints b_iq
+    b_iq = [];
+    for ind = 1:forces*N
+        % alpha = gait(ind);
+        alpha = 1;
+        b_iq = [b_iq; 0; 0; 0; 0; alpha*F_max; -alpha*F_min];
+    end
+
+    % Linearization
+    % [Yaw, Pitch, Roll]
+    psi = ypr(1);
+    theta = ypr(2);
+    phi  = ypr(3);
+
+    Rz_T = [cos(psi), sin(psi), 0; -sin(psi), cos(psi), 0; 0, 0, 1];
+    I_w = Rz_T'*I_b*Rz_T;
+
+    A_bar = zeros(bar_states);
+    A_bar(1:3, 3*2+1:3*3) = eye(3);
+    A_bar(3+1:3*2, 3*3+1:3*4) = Rz_T;
+    A_bar(3*3, bar_states) = -1;
+
+    % Position of feet in world frame
+    r_f1 = pf(1:3) - P;
+    r_f2 = pf(4:6) - P;
+    r_f3 = pf(7:9) - P;
+    r_f4 = pf(10:12) - P;
+    r_fs = [r_f1, r_f2, r_f3, r_f4];
+    temp_mat = [];
+    for ind = 1:forces
+        temp_r = r_fs(:, ind);
+        temp = [0, -temp_r(3), temp_r(2); temp_r(3), 0, -temp_r(1); -temp_r(2), temp_r(1), 0];
+        temp_term = inv(I_w)*temp;
+        temp_mat = [temp_mat, temp_term];
+    end
+    B_bar = [zeros(3*2, 3*forces); [eye(3)/m, eye(3)/m, eye(3)/m, eye(3)/m]; temp_mat; zeros(1, 3*forces)];
+
+    % Discretization
+    A_k = A_bar*dt + eye(bar_states);
+    B_k = B_bar*dt;
+
+    % MPC Dynamic Constraints
+    A_eq1 = zeros(bar_states, Xmpc_elem - 3*forces*N);
+    A_eq1(1:bar_states, 1:bar_states) = eye(bar_states);
+    A_Xrot = [-A_k, eye(bar_states)];
+    A_Xrot = [A_Xrot, zeros(bar_states, bar_states*(N-2))];
+    % Shift for each 
+    for ind = 1:N-1
+        A_eq1 = [A_eq1; A_Xrot];
+        A_Xrot = circshift(A_Xrot,bar_states, 2);
+    end
+    A_eq2 = [];
+    for ind = 1:N
+        temp_vec = zeros(Xmpc_elem - 3*forces*N, 1);
+        start = 1 + bar_states*(ind - 1);
+        temp_vec(start:start+bar_states-1, 1:3*forces) = -B_k; 
+        A_eq2 = [A_eq2, temp_vec];
+    end
+    A_eq = [A_eq1, A_eq2];
+
+    b_eq = zeros(Xmpc_elem - 3*forces*N, 1);
+    b_eq(1:bar_states) = A_k*X_bar;
+
+
+    X_mpc = quadprog(H, f, A_iq, b_iq, A_eq, b_eq);
+    grf_forces = X_mpc(N*bar_states + 1:N*bar_states + 3*forces, 1);
+end
---
+++ Final_Task_1/mpc_testing.m
@@ -0,0 +1,11 @@
+clear; clc;
+
+X = [zeros(3, 1); zeros(3,1); zeros(3,1); zeros(3, 1)];
+pf = [1; 1; 0; 1; -1; 0; -1; 1; 0; -1; -1; 0];
+Xd = [0; 0; 0.3; zeros(3,1); zeros(3,1); zeros(3, 1)];
+
+gait = 'standing';
+
+%mpc(X, pf, Xd, gait) 
+
+mpc_simulink(X, pf) 
