eb6f9ee2aab30ddfa0882c5ba339b545830be97a
mkuznets23 <77693398+mkuznets23@users.noreply.github.com>
2024-04-22 19:05:50 -0700

    integrating lococontroller into simulink

--- Final_Task_1/locoController.m
+++ Final_Task_1/locoController.m
@@ -5,26 +5,31 @@ gaitperiod = 0.06;
 legs = 4;
 rrf = zeros(12, 1);
 
-[currcontact, ftcontacts] = project_gait(t,N,dt, gaitperiod, gaitname);
+localSwingTimer = zeros(4,1);
 
+[currcontact, ftcontacts] = project_gait(t,N,dt, gaitperiod, gaitname);
 % If current and future contacts are all 1, do standing PD
 if all(currcontact == 1) && all(ftcontacts == 1)
     qDes = stand(t);
     dqDes = zeros(12, 1);
     u = jointPD(qDes, q, dqDes, dq);
-    for ind = 1:legs
-        start_ind = ind*3 - 2;
-        end_ind = ind*3;
-        rrf(start_ind:end_ind) = torque_to_force(u(start_ind:end_ind), q(start_ind:end_ind));
-    end
+    rrf = torque_to_force(u, q);
+    disp('standing')
 % Else, run mpc
 else
+    disp('mpc')
     % If leg starts swing phase, run swing control for it
     rrf_swing = zeros(12, 1);
     ftcontact_next = ftcontacts(1:legs);
     for ind = 1:legs
-        if currcontact(ind) == 1 && ftcontact_next(ind) == 0
-            %rrf_swing(ind*3 - 2: ind*3) = SWING CONTROL for the specific leg
+        if ftcontact_next(ind) == 0
+            if currcontact == 1
+            % reset local timer to 0
+                localSwingTimer(ind) = 0;
+            end
+            % iterate swing with local timer
+            % [pf, dpf] = swing_trajectory()
+            % rrf_swing(ind*3 - 2: ind*3) = swing_control()
         end
     end
 
--- Final_Task_1/torque_to_force.m
+++ Final_Task_1/torque_to_force.m
@@ -2,54 +2,89 @@
 % Inputs: 
 %   torques - joint torque (3x1)
 %   q - joints angles (3x1 in radians)
-function F = torque_to_force(torques, q)
+function F = torque_to_force(torques, Q)
+    offset_x = 0.1805;
+    offset_y = -0.047;
+    offset_z = 0.0;
+    F = zeros(12,1);
     % Kinematic parameters
-    a1 = 0.1; %m
+    a1 = 0.045; %m
     a2 = 0.2; %m
     a3 = 0.2; %m
+    
+    % iterate through legs. Order is FL,FR,RL,RF
+    for i = 0:3
 
-    q1 = q(1);
-    q2 = q(2);
-    q3 = q(3);
+        legSign = 1;
+        if i == 0 || i == 2
+            legSign = -1;
+        end
+        
+        frontBackSign = 1;
+        if i == 2 || i == 3
+            frontBackSign = -1;
+        end
+
+        q = Q(3*i+1:3*i+3);
+        f = F(3*i+1:3*i+3);
+
+        % s1 = sin(q(1));
+        % s2 = sin(q(2));
+        % s3 = sin(q(3));
+        % 
+        % c1 = cos(q(1));
+        % c2 = cos(q(2));
+        % c3 = cos(q(3));
+        %transformation from body frame to hip frame
+        % R_B0 = eul2rotm([-pi/2,0,-pi/2]);
+        R_B0 = [0 0 1;%for coordinate flipping, before q1 applied
+            -1 0 0;
+             0 -1 0];
+        % d_BodyToHip = [offset_x * frontBackSign; offset_y * legSign; offset_z];
+        % T_B0 = [R_B0,d_BodyToHip;zeros(1,3),1];
+        T_B0 = [R_B0,zeros(3, 1);zeros(1,3),1];
+        
+        %transformation from hip frame to thigh frame
+        % R = eul2rotm([pi/2,0,pi/2]); 
+        R = [0 0 1;%for coordinate flipping, before q1 applied
+            1 0 0;
+             0 1 0];
+        R_T = [R,[0;0;0];zeros(1,3),1];
+        T_01rot = [cos(q(1)) -sin(q(1)) 0 0; sin(q(1)) cos(q(1)) 0 0 ; 0 0 1 0; 0 0 0 1];
+        T_01 = T_01rot * R_T; %combine coordinate flip and q1 application
+        
+        %transformation from thigh to knee frame
+        T_12trans = eye(4);
+        T_12trans(1:3,4) = [a2; 0; a1*legSign];
+        T_12rot = [cos(q(2)) -sin(q(2)) 0 0; sin(q(2)) cos(q(2)) 0 0 ; 0 0 1 0; 0 0 0 1];
+        T_12 = T_12rot * T_12trans;
+        
+        %transformation from knee to toe frame
+        T_23trans = eye(4);
+        T_23trans(1:3,4) = [a3; 0; 0];
+        T_23rot = [cos(q(3)) -sin(q(3)) 0 0; sin(q(3)) cos(q(3)) 0 0 ; 0 0 1 0; 0 0 0 1];
+        T_23 = T_23rot * T_23trans;
+        
+        %compute frame origins (all in body frame)
+        P_EE = [0;0;0;1];
+        O_0 = T_B0* P_EE;
+        O_1 = T_B0* T_01 * P_EE;
+        O_2 = T_B0* T_01 * T_12 * P_EE;
+        O_3 = T_B0* T_01 * T_12 * T_23 * P_EE;
+        
+        %compute joint axes (all in body frame)
+        z0 = [1;0;0];
+        z1 = [0;-cos(q(1));-sin(q(1))];
+        z2 = [0;-cos(q(1));-sin(q(1))];
+        
+        %compute Jacobian
+        J = [cross(z0, O_3(1:3)-O_0(1:3)), ...
+            cross(z1, O_3(1:3)-O_1(1:3)), ...
+            cross(z2, O_3(1:3)-O_2(1:3))];
+        
+        torque = torques(3*i +1 : 3*i + 3);
+
+        F(3*i+1:3*i+3) = J * torque; %.* [1;-1;1];
+    end
     
-    %transformation from body frame to hip frame
-    R_B0 = eul2rotm([-pi/2,0,-pi/2]);
-    T_B0 = [R_B0,[0;0;0];zeros(1,3),1];
-    
-    %transformation from hip frame to thigh frame
-    R = eul2rotm([pi/2,0,pi/2]); %for coordinate flipping, before q1 applied
-    R_T = [R,[0;0;0];zeros(1,3),1];
-    T_01rot = [cos(q1) -sin(q1) 0 0; sin(q1) cos(q1) 0 0 ; 0 0 1 0; 0 0 0 1];
-    T_01 = T_01rot * R_T; %combine coordinate flip and q1 application
-    
-    %transformation from thigh to knee frame
-    T_12trans = eye(4);
-    T_12trans(1:3,4) = [a2; 0; a1];
-    T_12rot = [cos(q2) -sin(q2) 0 0; sin(q2) cos(q2) 0 0 ; 0 0 1 0; 0 0 0 1];
-    T_12 = T_12rot * T_12trans;
-    
-    %transformation from knee to toe frame
-    T_23trans = eye(4);
-    T_23trans(1:3,4) = [a3; 0; 0];
-    T_23rot = [cos(q3) -sin(q3) 0 0; sin(q3) cos(q3) 0 0 ; 0 0 1 0; 0 0 0 1];
-    T_23 = T_23rot * T_23trans;
-    
-    %compute frame origins (all in body frame)
-    P_EE = [0;0;0;1];
-    O_0 = T_B0* P_EE;
-    O_1 = T_B0* T_01 * P_EE;
-    O_2 = T_B0* T_01 * T_12 * P_EE;
-    O_3 = T_B0* T_01 * T_12 * T_23 * P_EE;
-    
-    %compute joint axes (all in body frame)
-    z0 = [1;0;0];
-    z1 = [0;-cos(q1);-sin(q1)];
-    z2 = [0;-cos(q1);-sin(q1)];
-    
-    %compute Jacobian
-    J = [cross(z0, O_3(1:3)-O_0(1:3)), ...
-        cross(z1, O_3(1:3)-O_1(1:3)), ...
-        cross(z2, O_3(1:3)-O_2(1:3))];
-    
-    F = J * torques;
 end
