a4fb72017d3e6049f7038ea8e8308da14a69d51e
liyvonne <79174603+liyvonne@users.noreply.github.com>
2024-05-01 14:45:57 -0700

    mpc troubleshoot idk

--- Final_Task_1/mpc_simulink.m
+++ Final_Task_1/mpc_simulink.m
@@ -11,15 +11,15 @@ bar_states = 13;
 % dt = 0.03;
 Xmpc_elem = bar_states*N + legs*3*N;
 % Q, R values from HW4 #2(b)
-% Q = diag([40, 50, 60, 10, 10, 10, 4, 4, 4, 1, 1, 1, 0]);
+ Q = diag([40, 50, 60, 10, 10, 10, 4, 4, 4, 1, 1, 1, 0]);
 
 % test tuning for trotting in place
-Q = diag([40, 50, 60, 10, 10, 10, 4, 4, 4, 1, 1, 1, 0]);
+%Q = diag([10, 10, 10, 30, 30, 30, 4, 4, 4, 1, 1, 1, 0]);
 
 % test tuning for trotting forward
 % Q = diag([40, 50, 60, 10, 10, 10, 4, 4, 4, 1, 1, 1, 0]);
 
-R = 0.00005*eye(legs*3);
+R = 0.00001*eye(legs*3);
 m = 12;
 F_max = 500;
 F_min = 10;
