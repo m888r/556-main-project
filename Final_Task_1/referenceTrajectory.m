dca89dd6d5b949a4d3cdcbd366387d01e34d83d7
m888r <25947228+m888r@users.noreply.github.com>
2024-05-02 11:57:50 -0700

    change ref trajectory to also use current state as a reference

--- Final_Task_1/referenceTrajectory.m
+++ Final_Task_1/referenceTrajectory.m
@@ -7,15 +7,15 @@
     so that's added in here
 %}
 
-function [X_traj] = referenceTrajectory(Xd, N, dt)
+function [X_traj] = referenceTrajectory(Xd, X, N, dt)
 
     X_traj = zeros(13*N, 1);
 
     for i = 1:N
         vel_new = Xd(7:9);
-        pos_new = Xd(1:3) + vel_new * dt * i;
+        pos_new = X(1:3) + vel_new * dt * i;
         angvel_new = Xd(10:12);
-        ang_new = Xd(4:6) + angvel_new * dt * i;
+        ang_new = X(4:6) + angvel_new * dt * i;
         X_traj((i-1)*12+1:i*12) = [pos_new; ang_new; vel_new; angvel_new; 9.81];
     end
 end
