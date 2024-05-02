53096a1cf55da3fce2f9565f073badf8a82c661d
m888r <25947228+m888r@users.noreply.github.com>
2024-05-02 12:12:13 -0700

    changed ref trajectory to add 9.81

--- Final_Task_1/referenceTrajectory.m
+++ Final_Task_1/referenceTrajectory.m
@@ -4,19 +4,19 @@
     set angular and linear velocities in the Xd vector
     Xd = [pos; ang; vel; angvel]
     note: this MPC formulation uses Xd = [pos; ang; vel; angvel; 9.81]
-    so that's added in here
 %}
 
 function [X_traj] = referenceTrajectory(Xd, X, N, dt)
 
-    X_traj = zeros(13*N, 1);
+    Xd_len = 13;
+    X_traj = zeros(Xd_len*N, 1);
 
     for i = 1:N
         vel_new = Xd(7:9);
         pos_new = X(1:3) + vel_new * dt * i;
         angvel_new = Xd(10:12);
         ang_new = X(4:6) + angvel_new * dt * i;
-        X_traj((i-1)*13+1:i*13) = [pos_new; ang_new; vel_new; angvel_new; 9.81];
+        X_traj((i-1)*Xd_len+1:i*Xd_len) = [pos_new; ang_new; vel_new; angvel_new; 9.81];
     end
 
 end
