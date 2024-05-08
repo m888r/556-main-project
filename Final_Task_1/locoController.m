fdf62e5687ebe2e67c514ca390584b200b8fe9c3
liyvonne <79174603+liyvonne@users.noreply.github.com>
2024-05-07 19:02:37 -0700

    turn in place Q tune

--- Final_Task_1/locoController.m
+++ Final_Task_1/locoController.m
@@ -29,7 +29,7 @@ curr_pf_target = zeros(12, 1);
 gaitname = gaitScheduler(X, pf, t);
 
 % ypr angles (gets rearranged in mpc_simulink)
-walking_x_Q = [10, 10, 30, 30, 600, 150, 20, 4, 4, 20, 1, 1, 0];
+walking_x_Q = [10, 10, 30, 30, 600, 150, 20, 4, 4, 100, 1, 1, 0];
 bounding_Q = [90, 40, 3000, 30, 1, 30, 4, 4, 4, 1, 1, 1, 0];
 walking_x_Kstep = 0.1;
 
@@ -67,7 +67,7 @@ else
     v_x_des = 0;
     %v_y_des = speed_ramp(t, 0.65, 2, 0, 1);
     v_y_des = 0;
-    yaw_des = speed_ramp(t, 0.65, 2, 0, 0.5);
+    yaw_des = speed_ramp(t, 0.65, 2, 0, 1);
     
     walking_Xd = [X(1); X(2); 0.25; X(4); 0; 0; v_x_des; v_y_des; 0; yaw_des; 0; 0];
     
