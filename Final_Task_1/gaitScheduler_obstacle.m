e448b453224e5c54c849b6322a0726ad4b82be36
mkuznets23 <77693398+mkuznets23@users.noreply.github.com>
2024-05-08 12:17:11 -0700

    obstacle finalized

--- Final_Task_1/gaitScheduler_obstacle.m
+++ Final_Task_1/gaitScheduler_obstacle.m
@@ -1,4 +1,4 @@
-function [gaitname, landHeight, jumpVel, jumpAngle, walkVel, height, R_f, x_Q, pf_target] = gaitScheduler_obstacle(X, pf, t)
+function [gaitname, landHeight, jumpVel, jumpAngle, walkVel, height, R_f, x_Q, pf_target,soarPD] = gaitScheduler_obstacle(X, pf, t)
 persistent state2_start;
 persistent state3_start;
 persistent obstacleState;
@@ -21,7 +21,7 @@ landHeight = 0;
 % trot until reaching certain X, do jump, then continue trotting
 jumpAngle = 0;
 jumpVel = [0;0;0];
-
+soarPD = 0;
 height = 0;
 walkVel = 0;
 R_f = 0.00005;
@@ -32,7 +32,7 @@ pf_target = [0.25;0.14;-0.1;
     -0.15;0.14;-0.1;
     -0.15;-0.14;-0.1];
 
-if X(1) > 0.3%0.7 % right before first obstacle
+if X(1) > 0.64%0.7 % right before first obstacle
     obstacleState = 2;
     
     if state2_start == 0 %first iteration that state2 starts
@@ -40,7 +40,7 @@ if X(1) > 0.3%0.7 % right before first obstacle
     end
 end
 
-if X(1) > 5.62%5.68
+if X(1) > 5.58%5.68
     obstacleState = 3;
     
     if state3_start == 0  %first iteration that state3 starts
@@ -89,42 +89,56 @@ if obstacleState == 1
 elseif obstacleState == 2
     % jumping over first obstacle and trotting to last obstacle
     
-    if t-state2_start < 0.45
+    deccelerate = 0.9;
+    trotInPlace = 0.15;
+    standing = 0.3;
+    jumping = 0.15;
+    soaring = 0.35; %0.45
+    landing = 0.35;
+    jumping2 = 0.15;
+    soaring2 = 0.4;
+    landing2 = 0.6;
+    
+    if t-state2_start < deccelerate
         gaitname = "trotting";
         height = 0.25;
-        walkVel = speed_ramp(t, 0, 0.4, 0.7, 0);
+        walkVel = 0.7+speed_ramp(t, 0, deccelerate, 0, -0.7);
         disp("second trot")
-    elseif t-state2_start < 0.6
+    elseif t-state2_start < deccelerate + trotInPlace
         gaitname = "trotting";
         height = 0.25;
         walkVel = 0;
         disp("second trot")
-    elseif t-state2_start < 0.65
+    elseif t-state2_start < deccelerate + trotInPlace + standing
         gaitname = "standing";
-    elseif t-state2_start < 0.8
+    elseif t-state2_start < deccelerate + trotInPlace + standing + jumping
         gaitname = "jumpingg";
         jumpVel = [2;0;3];
-        jumpAngle = -pi/8; %-pi/4;
+        jumpAngle = -pi/4; %-pi/4;
         R_f = 0.00005;
-    elseif t-state2_start < 1.15
+    elseif t-state2_start < deccelerate + trotInPlace + standing + jumping + soaring
         gaitname = "soaringg";
-    elseif t-state2_start < 2
+        soarPD = 1;
+    elseif t-state2_start <  deccelerate + trotInPlace + standing + jumping + soaring + landing
         gaitname = "landingg";
         landHeight = 0.4;
-    elseif t-state2_start < 2.15
+    elseif t-state2_start < deccelerate + trotInPlace + standing + jumping + soaring + landing + jumping2
         gaitname = "jumpingg";
         jumpVel = [2;0;3];
         jumpAngle = 0;
         R_f = 0.00005;
-    elseif t-state2_start < 2.4
+    elseif t-state2_start < deccelerate + trotInPlace + standing + jumping + soaring + landing + jumping2 + soaring2
         gaitname = "soaringg";
-    elseif t-state2_start < 3
+        soarPD = 1;
+    elseif t-state2_start < deccelerate + trotInPlace + standing + jumping + soaring + landing + jumping2 + soaring2 + landing2
         gaitname = "landingg";
         landHeight = 0.2;
     else
         gaitname = "trotting";
         height = 0.25;
-        walkVel = speed_ramp(t-state2_start, 3, 4, 0, 1.5);
+        walkVel = speed_ramp(t-state2_start, ...
+            deccelerate + trotInPlace + standing + jumping + soaring + landing + jumping2 + soaring2 + landing2 ...
+            , deccelerate + trotInPlace + standing + jumping + soaring + landing + jumping2 + soaring2 + landing2 + 1, 0, 1.5);
         % walkVel = 0.5;
     end
 else
@@ -133,8 +147,9 @@ else
     trotInPlace = 0.6;
     standing = 0.3;
     jumping = 0.15;
-    soaring = 0.45;
-    landing = 0.5;
+    soaring = 0.75; %0.45
+    soarPD_begin = 0.65;
+    landing = 0.3;
     
     if t-state3_start < deccelerate
         gaitname = "trotting";
@@ -159,10 +174,22 @@ else
         R_f = 0.000005;
     elseif t-state3_start < deccelerate + trotInPlace + standing + jumping + soaring
         gaitname = "soaringg";
-        pf_target = [0.25;0.14;-0.05;
-            0.25;-0.14;-0.05;
-            -0.15;0.14;-0.05;
-            -0.15;-0.14;-0.05];
+        
+        soarPD = 1;
+        
+        pf_target = [-0.15;0.14;-0.0;
+            -0.15;-0.14;-0.0;
+            -0.25;0.14;-0.05;
+            -0.25;-0.14;-0.05];
+        
+        
+        if t-state3_start > deccelerate + trotInPlace + standing + jumping + soarPD_begin
+            
+            pf_target = [0.25;0.14;-0.25;
+                0.25;-0.14;-0.25;
+                -0.15;0.14;-0.3;
+                -0.15;-0.14;-0.3];
+        end
     elseif t-state3_start < deccelerate + trotInPlace + standing + jumping + soaring + landing
         gaitname = "landingg";
         landHeight = 0.2;
